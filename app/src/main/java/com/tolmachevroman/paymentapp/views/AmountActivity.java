192c795687e284ba970fe69eaae4e991ea4d218a
tolmachevroman <tolmachevroman@gmail.com>
2017-12-12 13:12:37 -0300

    packages restructured

--- app/src/main/java/com/tolmachevroman/paymentapp/views/AmountActivity.java
+++ app/src/main/java/com/tolmachevroman/paymentapp/views/AmountActivity.java
@@ -1,7 +1,9 @@
-package com.tolmachevroman.paymentapp;
+package com.tolmachevroman.paymentapp.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tolmachevroman.paymentapp.R;
 
 public class AmountActivity extends AppCompatActivity {
 
