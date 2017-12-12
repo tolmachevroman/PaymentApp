bf88c29f4d58cbcfb235c227bccf423dbf17ee1a
tolmachevroman <tolmachevroman@gmail.com>
2017-12-12 14:44:00 -0300

    onContinueClicked logic

--- app/src/main/java/com/tolmachevroman/paymentapp/views/AmountActivity.java
+++ app/src/main/java/com/tolmachevroman/paymentapp/views/AmountActivity.java
@@ -1,15 +1,52 @@
 package com.tolmachevroman.paymentapp.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tolmachevroman.paymentapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class AmountActivity extends AppCompatActivity {
+
+    @BindView(R.id.amountTxt)
+    EditText amountTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amount);
+        ButterKnife.bind(this);
+
+        amountTxt.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    onContinueClicked(null);
+                    return true;
+                }
+                return false;
+            }
+
+        });
+    }
+
+    @OnClick(R.id.continueBtn)
+    public void onContinueClicked(View view) {
+        String amount = amountTxt.getText().toString();
+        if (amount.isEmpty()) {
+            Toast.makeText(this, getString(R.string.input_amount_toast), Toast.LENGTH_SHORT).show();
+        } else {
+            //TODO go to the next activity
+        }
     }
 }
